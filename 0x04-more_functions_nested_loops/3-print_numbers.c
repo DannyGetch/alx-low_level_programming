a77d79946d5c3cb5189385374013ac92e4884d7d
Danny <dannygetch@gmail.com>
2022-04-14 05:36:47 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbeers - Prints 0 to 9
+ * print_numbers - Prints 0 to 9
  *
  * Return: Always 0 (Success)
  */
 
-void print_numbers()
+void print_numbers(void)
 {
 	int i = 0;
 
