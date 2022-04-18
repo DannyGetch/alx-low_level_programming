434749c23b41bb97a73f1802770c630fc3a4b813
Danny <dannygetch@gmail.com>
2022-04-18 06:28:43 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ *
+ * @str: the string to be processed
+ *
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	while (j <= i)
+	{
+		putchar(str[j]);
+		j += 2;
+	}
+	putchar('\n');
+}
